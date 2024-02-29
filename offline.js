fe1e3da12065d318b5b4f85ca3bf174c654586e6
royan28 <161667601+royan28@users.noreply.github.com>
2024-02-29 09:44:45 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1709172272,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/whatsappimage20240215at-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/supermariopipepixelartmaker25996removebgpreview-sheet0.png",
+		"images/bawah-sheet0.png",
+		"images/atas-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
